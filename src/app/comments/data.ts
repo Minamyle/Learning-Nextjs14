4a0e91d6884ab51eed6d04d42e23aeb7db1828a7
Amina Ismaila <minamyle@gmail.com>
2024-08-28 03:08:25 +0100

    feat: handle GET request

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id:1,
+        text:"this is the first comments"
+    },
+    {
+        id:2, 
+        text:" this is the second comments"
+    },
+    {
+        id:3, 
+        text:"this is the third comments"
+    },
+]
