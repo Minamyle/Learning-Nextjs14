d42f68748b6ebebe49fa9a7759fd847bf57a2d0f
Amina Ismaila <minamyle@gmail.com>
2024-08-28 05:24:02 +0100

    feat:cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,13 +1,20 @@
 import { type NextRequest } from "next/server"
-import { headers } from 'next/headers'
+import { headers, cookies } from 'next/headers'
+
 export async function GET(request:NextRequest){
     const requestHeaders = new Headers(request.headers)
     const headerList = headers();
+    cookies().set("resultsPerPage", "20");
+
+    const theme = request.cookies.get("theme") 
+    console.log(theme)
+    console.log(cookies().get("resultsPerPage"))
     console.log(requestHeaders.get("Authorization"));
     console.log(headerList.get("Authorization"));
     return new Response("<h1>profile api data</h1>", {
         headers: {
             "content-Type":"text/html",
+            "Set-cookie":"theme=dark",
         }
     }) 
 }
