097a8ffd6a3def5cf89d39903716378c1ce67bd3
Amina Ismaila <minamyle@gmail.com>
2024-08-28 05:13:02 +0100

    feat:headers in route handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,13 @@
-export async function GET(){
-    return new Response("profile api data") 
+import { type NextRequest } from "next/server"
+import { headers } from 'next/headers'
+export async function GET(request:NextRequest){
+    const requestHeaders = new Headers(request.headers)
+    const headerList = headers();
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+    return new Response("<h1>profile api data</h1>", {
+        headers: {
+            "content-Type":"text/html",
+        }
+    }) 
 }
