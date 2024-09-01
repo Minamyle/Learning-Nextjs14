c4e948ce922ccafade347d061a900f90c6ed288b
Amina Ismaila <minamyle@gmail.com>
2024-09-02 00:14:01 +0100

    feat:error state

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -8,7 +8,7 @@ type User = {
 
 export default async function Users() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch("https://jsonplaceholder.typicode.com/users1");
   const users = await response.json();
   console.log(users);
   return (
